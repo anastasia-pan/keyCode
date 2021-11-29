76643d4b92b251f3a8c6f8e1c2179cea9dc0f78c
Anastasia Panoutsou <panoutsou.anastasia@gmail.com>
2021-11-29 21:15:25 +0000

    hid the text boxes until a key is pressed

--- app.js
+++ app.js
@@ -4,11 +4,17 @@ let mainboxNumber = document.getElementById("mainboxNumber");
 let keyValue = document.getElementById("keyValue");
 let keyNumber = document.getElementById("whichNumber");
 let keyCode = document.getElementById("keyCodevalue");
+let boxKey = document.getElementById("eventKey");
+let boxWhich = document.getElementById("eventWhich");
+let boxCode = document.getElementById("keyCode");
 
 // event listener for keypress
 document.addEventListener("keypress", (event) => {
   //command to hide the text from the main display
   mainboxText.style.display = "none";
+  boxKey.style.display = "block";
+  boxWhich.style.display = "block";
+  boxCode.style.display = "block";
   //mainboxNumber and keyNumber populated with event.which result
   mainboxNumber.textContent = event.which;
   keyNumber.textContent = event.which;
